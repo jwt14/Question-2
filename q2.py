4ffb99c8741f875a06f732ced4478094ab7904eb
jwt14 <jwt14@ic.ac.uk>
2016-03-26 16:30:50 +0100

    v0.6
    Added analytic solution to a sinusoid heat distribution

--- q2.py
+++ q2.py
@@ -4,7 +4,7 @@
 import subprocess
 import numpy as np
 import matplotlib.pyplot as plt
-from math import pi,sin
+from math import pi,sin,exp
 #Any of these parameters can be changed by user
 
 L = 1
@@ -16,6 +16,7 @@ d_x = L/float(N_x)
 theta = 0
 alpha = 1
 heat_mid = np.array(sin(pi*0.5*L))               #Operatre on Numpy arrays for maximum efficiency
+heat_analytic = np.array(sin(pi*0.5*L))               #Operatre on Numpy arrays for maximum efficiency
 time = np.linspace(0, 1, N_t+1)
 
 
@@ -27,6 +28,11 @@ a=data.split('\n')
 for i in range(1, N_t+1):
     b = eval(a[int((0.5*N_x+1)*(2*i-1))])
     heat_mid = np.append(heat_mid, b)
+
+for j in range(1, N_t+1):
+    c = sin(pi*0.5*L)*exp(-alpha*pi**2*j*d_t/L/L)
+    heat_analytic = np.append(heat_analytic, c)
+
 
 fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
 axes[0].plot(time, heat_mid, 'r')
