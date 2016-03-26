eacd9eb4af58318999ed89061f3eb1a631f95ccb
jwt14 <jwt14@ic.ac.uk>
2016-03-26 14:23:21 +0100

    v0.21
    Improved layout by introduing 'format' printing style.

--- q2.py
+++ q2.py
@@ -1,4 +1,15 @@
+#   HPC Q2 
+#   Created by Jan Witold Tomaszewski CID: 00833865
+
 import subprocess
 
+#Any of these parameters can be changed by user
+L = 1
 N_x = 10
-data = subprocess.Popen('./q2 1 ' + str(N_x) + ' 1 10000 1', stdout=subprocess.PIPE).communicate()[0]
+T = 1
+N_t = 10000
+alpha = 1
+
+
+data = subprocess.Popen("./q2 {} {} {} {} {}".format(L, N_x, T, N_t, alpha),
+                        stdout=subprocess.PIPE).communicate()[0]
